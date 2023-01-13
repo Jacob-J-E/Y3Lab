d23c857d80b57fcd57b9be62c49741d31da36820
Jacob-J-E <jacobedginton@gmail.com>
2023-01-13 11:53:37 +0000

    Filter

--- X-Ray/test.py
+++ X-Ray/test.py
@@ -6,18 +6,22 @@ import mplhep as hep
 hep.style.use("ATLAS")
 
 
-data = pd.read_csv(r"C:\Users\Ellre\Desktop\Physics Degree\LAB\Year 3 Lab\X Ray Diffraction\Data\NaCl Initial.csv",skiprows=1)
+data = pd.read_csv(r"X-Ray\Data\NaCl_Initial.csv",skiprows=0)
 print(data)
+data = data.rename(columns={"&b / ":"angle","R_0 / 1/s":"count"})
 
+angle = data["angle"]
+count = data["count"]
 
-
+x = angle
+y = count
 # x = np.linspace(0,2*np.pi,100)
 # y = np.sin(x) + np.random.random(100) * 0.2
-# yhat = savgol_filter(y, 51, 3) # window size 51, polynomial order 3
+yhat = savgol_filter(y, 51, 3) # window size 51, polynomial order 3
 
-# plt.plot(x,y)
-# plt.scatter(x,yhat, color='red')
-# plt.grid()
-# plt.xlabel(r"Scattering Angle \ $\deg$")
-# plt.ylabel(r"Count Rate \ $s^{-1}$")
-# plt.show()
+plt.plot(x,y)
+plt.plot(x,yhat, color='red')
+plt.grid()
+plt.xlabel(r"Scattering Angle \ $\deg$")
+plt.ylabel(r"Count Rate \ $s^{-1}$")
+plt.show()
