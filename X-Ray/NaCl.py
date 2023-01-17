ee1daa8bb6dd0677b3b61e636ac48750851c89d0
Jacob-J-E <jacobedginton@gmail.com>
2023-01-17 10:17:12 +0000

    Comments

--- X-Ray/NaCl.py
+++ X-Ray/NaCl.py
@@ -4,16 +4,19 @@ import matplotlib.pyplot as plt
 import mplhep as hep
 hep.style.use("ATLAS")
 
-
+# Load in data
 data = pd.read_csv(r"X-Ray\Data\16-01-2022\NaCl Full Data.csv",skiprows=0)
 print(data)
 
-
+# Split data into differnt variables
 angle = data['angle']
 wav = data['wav / pm']
 energy = data['E / keV']
 count_0 = data['R_0 / 1/s']
 
+
+
+#Initial Plots
 fig, ax = plt.subplots(3)
 ax[0].plot(angle,count_0)
 ax[1].plot(wav,count_0)
@@ -28,3 +31,6 @@ ax[1].set_ylabel(r"Count $(s^{-1})$")
 ax[2].set_ylabel(r"Count $(s^{-1})$")
 plt.tight_layout()
 plt.show()
+
+
+
