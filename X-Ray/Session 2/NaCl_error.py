7401c5a2916a63a908ec3f409606bcad730b6d40
Jacob-J-E <jacobedginton@gmail.com>
2023-01-17 10:59:14 +0000

    Error Prop Formula

--- X-Ray/Session 2/NaCl_error.py
+++ X-Ray/Session 2/NaCl_error.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 import mplhep as hep
 hep.style.use("ATLAS")
 
+# Bragg scattering error propagation function
+def sigma_wav(m,d,theta,sig_theta,sig_d):
+    der_d = 2 * np.sin(theta) / m
+    der_theta = 2 * d * np.cos(theta) / m
+    return np.sqrt((der_d * sig_d)**2 + (der_theta * sig_theta))
+
+
 # Load in data
 data = pd.read_csv(r"X-Ray\Data\16-01-2022\NaCl Full Data.csv",skiprows=0)
 print(data)
