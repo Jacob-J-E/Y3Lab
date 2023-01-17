9c8c41aa6e38cca1b0817ee6f4d8fe9ec40199eb
Jacob-J-E <jacobedginton@gmail.com>
2023-01-17 09:57:44 +0000

    NaCl Plots

---
+++ NaCl.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
+import mplhep as hep
+hep.style.use("ATLAS")
+
+
+data = pd.read_csv(r"X-Ray\Data\16-01-2022\NaCl Full Data.csv",skiprows=0)
+print(data)
+
+
+angle = data['angle']
+wav = data['wav / pm']
+energy = data['E / keV']
+count_0 = data['R_0 / 1/s']
+
+fig, ax = plt.subplots(3)
+ax[0].plot(angle,count_0)
+ax[1].plot(wav,count_0)
+ax[2].plot(np.sort(energy),count_0)
+
+
+plt.show()
