72ce50aa7d6b403ac9445e2a7aaff91ca03f064c
Jacob-J-E <jacobedginton@gmail.com>
2023-01-23 11:57:06 +0000

    Moseley

--- X-Ray/Session 5/moseley.py
+++ X-Ray/Session 5/moseley.py
@@ -10,10 +10,10 @@ R_0 = 10973731.6
 def line(x,m,c):
     return m*x + c
 
-Z = np.array([40,47,42])
+Z = np.array([40,42,47])
 
-wav_alpha = np.array([66.5,46.5,59.5]) * 10e-12
-wav_beta = np.array([135,95,122])* 10e-12
+wav_alpha = np.array([66.5,59.5,46.5]) * 10e-12
+wav_beta = np.array([135-5,122-3,95-5])* 10e-12
 
 alpha_sigma = np.array([3,3,3])*10e-12
 
@@ -39,6 +39,8 @@ print(f"Rydberg 2: {beta_params[0]**2} Percent differnce {100*(beta_params[0]**2
 print(f"Screening 1: {-1*alpha_params[1]/alpha_params[0]}")
 print(f"Screening 2: {-1*beta_params[1]/beta_params[0]}")
 
+print(f"Sigma Ryd {alpha_cov[0][0]**2}")
+print(f"Sigma Ryd Frac {100*alpha_cov[0][0]**2/alpha_params[0]**2}")
 
 
 plt.scatter(Z,inv_sqrt_wav_alpha,color='red')
