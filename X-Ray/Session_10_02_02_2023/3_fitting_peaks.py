e8227f99aed718d856aa07c925a04329cfcd0c8c
Ganel <Ganelreddy1@gmail.com>
2023-02-03 13:54:37 +0000

    new data

--- X-Ray/Session_10_02_02_2023/3_fitting_peaks.py
+++ X-Ray/Session_10_02_02_2023/3_fitting_peaks.py
@@ -7,6 +7,8 @@ from scipy.signal import argrelextrema
 import xraydb
 import pickle
 
+
+
 #Loading in old low res data
 data_low_res = pd.read_csv(r"X-Ray\Data\24-01-2023\Plates Mo Source.csv",skiprows=0)
 
@@ -52,7 +54,9 @@ column_names_data_high_res_search = ['Mo plate cal',
 k_line_high_res_search = ['Mo plate cal','Indium','Pure Nickel']
 k_elements_high_res = ['Mo', 'In', 'Ni']
 
-l_line_high_res_search = ['Unknown - Tungsten (W)', 'Pb' 'Gold (Ganel’s pendant)']
+k_elements_in_order = k_elements_low_res + k_elements_high_res
+
+l_line_high_res_search = ['Unknown - Tungsten (W)', 'Pb', 'Gold (Ganel’s pendant)']
 l_elements_high_res = ['W', 'Pb', 'Au']
 
 high_res_atomic_number = [42,74,22,82, 26, 79, 50,49, 28]
@@ -60,10 +64,13 @@ high_res_atomic_number = [42,74,22,82, 26, 79, 50,49, 28]
 def gaussian(x, a, b, c,e,f,g):
     return (a * np.exp(-((x - b) ** 2) / (2 * c ** 2)) + e * np.exp(-((x - f) ** 2) / (2 * g ** 2)))
 
+def gaussian_triple(x, a, b, c,e,f,g,h,i,j):
+    return (a * np.exp(-((x - b) ** 2) / (2 * c ** 2)) + e * np.exp(-((x - f) ** 2) / (2 * g ** 2)) + h * np.exp(-((x - i) ** 2) / (2 * j ** 2)))
+
 
 plot = True
 if plot == True:
-    Tot = len(k_line_low_res_search+k_line_high_res_search)
+    Tot = len(k_line_low_res_search+k_line_high_res_search+l_line_high_res_search)
     Cols = 4
     Rows = Tot // Cols 
     if Tot % Cols != 0:
@@ -105,6 +112,7 @@ for i,col_name in enumerate(k_line_low_res_search):
 
     guess_e1 = [amplitudes[0],mu_guess_e1,0.1,amplitudes[1],mu_guess_e2,0.1]
     params_e1, cov_e1 = spo.curve_fit(gaussian,energy_np,cu_plate_np,guess_e1)
+
     if params_e1[1] < params_e1[4]:
         alpha_energies.append(params_e1[1])
         beta_energies.append(params_e1[4])
@@ -128,7 +136,7 @@ for i,col_name in enumerate(k_line_low_res_search):
         ax.plot(energy_low_res_x,cu_plate, label = f'{k_elements_low_res[i]} Plate') 
         ax.plot(energy_low_res_x,gaussian(energy_low_res_x,*params_e1), label = f'Gaussian fit (mu_e1 = {params_e1[1]:.2f},mu_e2 = {params_e1[4]:.2f})')
         plt.axvline(mu_guess_e1, label = r'$k_{\alpha}$' + f' with energy {mu_guess_e1:.2f} keV', color = 'blue')
-        plt.axvline(mu_guess_e2, label = r'$k_{\alpha}$' + f' with energy {mu_guess_e2:.2f} keV', color = 'red')
+        plt.axvline(mu_guess_e2, label = r'$k_{\beta}$' + f' with energy {mu_guess_e2:.2f} keV', color = 'red')
         ax.legend(loc="upper right")
     j += 1
 
@@ -186,8 +194,81 @@ for i,col_name in enumerate(k_line_high_res_search):
         ax.plot(energy_high_res_x,cu_plate, label = f'{k_elements_high_res[i]} Plate') 
         ax.plot(energy_high_res_x,gaussian(energy_high_res_x,*params_e1), label = f'Gaussian fit (mu_e1 = {params_e1[1]:.2f},mu_e2 = {params_e1[4]:.2f})')
         plt.axvline(mu_guess_e1, label = r'$k_{\alpha}$' + f' with energy {mu_guess_e1:.2f} keV', color = 'blue')
-        plt.axvline(mu_guess_e2, label = r'$k_{\alpha}$' + f' with energy {mu_guess_e2:.2f} keV', color = 'red')
+        plt.axvline(mu_guess_e2, label = r'$k_{\beta}$' + f' with energy {mu_guess_e2:.2f} keV', color = 'red')
         ax.legend(loc="upper right")
     j += 1
 
+l_alpha_energies = []
+l_beta_energies = []
+l_gamma_energies = []
+
+for i,col_name in enumerate(l_line_high_res_search):
+    cu_plate = data_high_res[col_name]
+
+
+    la1 = 0
+    lb1 = 0
+    lg1 = 0
+    for name, line in xraydb.xray_lines(l_elements_high_res[i]).items():
+        if name == 'La1':
+            la1 = line.energy
+        elif name == 'Lb1':
+            lb1 = line.energy
+        elif name == 'Lg1':
+            lg1 = line.energy
+
+    mu_guess_e1 = la1/1e3
+    mu_guess_e2 = lb1/1e3
+    mu_guess_e3 = lg1/1e3
+
+
+    cu_plate_np = np.array(cu_plate.tolist())
+    energy_np = np.array(energy_high_res_x.tolist())
+    # Find local maxima using the argrelextrema function
+    local_maxima = argrelextrema(cu_plate.to_numpy(), np.greater)
+
+    amplitudes = []
+    for x in local_maxima[0]:
+        amplitudes.append(cu_plate_np[x])
+
+    not_sorted_amplitudes = amplitudes.copy()
+    amplitudes.sort(reverse=True)
+
+    guess_e1 = [amplitudes[0],mu_guess_e1,0.1,amplitudes[1],mu_guess_e2,0.1,amplitudes[2],mu_guess_e3,0.1]
+    params_e1, cov_e1 = spo.curve_fit(gaussian_triple,energy_np,cu_plate_np,guess_e1)
+
+    energy_val = [params_e1[1],params_e1[4],params_e1[7]]
+    energy_val = sorted(energy_val)
+
+    l_alpha_energies.append(energy_val[2])
+    l_beta_energies.append(energy_val[1]) 
+    l_gamma_energies.append(energy_val[0]) 
+
+
+
+    # print(f"mu_e1: {params_e1[1]}")
+    # print(f"sigma_e1: {params_e1[2]}")
+    # print(f"mu_e2: {params_e1[4]}")
+    # print(f"sigma_e2: {params_e1[5]}")
+
+    # print(f"cov mu_e1: {np.sqrt(cov_e1[0][0])}")
+    # print(f"covsigma_e1: {np.sqrt(cov_e1[1][1])}")
+    # print(f"cov mu_e2: {np.sqrt(cov_e1[2][2])}")
+    # print(f"covsigma_e2: {np.sqrt(cov_e1[3][3])}")
+
+    if plot == True:
+        ax = fig.add_subplot(Rows,Cols,Position[j])
+        ax.plot(energy_high_res_x,cu_plate, label = f'{k_elements_high_res[i]} Plate') 
+        ax.plot(energy_high_res_x,gaussian_triple(energy_high_res_x,*params_e1), label = f'Gaussian fit (mu_e1 = {params_e1[1]:.2f},mu_e2 = {params_e1[4]:.2f},mu_e3 = {params_e1[7]:.2f})')
+        plt.axvline(mu_guess_e1, label = r'$l_{\alpha}$' + f' with energy {mu_guess_e1:.2f} keV', color = 'blue')
+        plt.axvline(mu_guess_e2, label = r'$l_{\beta}$' + f' with energy {mu_guess_e2:.2f} keV', color = 'red')
+        plt.axvline(mu_guess_e3, label = r'$l_{\gamma}$' + f' with energy {mu_guess_e3:.2f} keV', color = 'green')
+        ax.legend(loc="upper right")
+    j += 1
+
+print(alpha_energies)
+print(beta_energies)
+print(l_alpha_energies)
+print(l_beta_energies)
+print(l_gamma_energies)
 plt.show()
---
+++ X-Ray/Session_10_02_02_2023/4_moseleys_law.py
@@ -0,0 +1,129 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.optimize as spo
+import pandas as pd
+import math
+from scipy.signal import argrelextrema
+import xraydb
+
+R_0 = 10973731.6
+
+def straight_line(x,m,c):
+    return m * x + c
+
+def cal_rydberg(m,factor):
+    return (1/factor)*m**2
+
+k_alpha = [8.080748431650838, 22.372552420613246, 16.044092033837604, 8.715570846388037, 4.57752299496841, 6.4190948273611665, 25.324555158862026, 17.421595970841373, 23.893491144556645, 7.681898645165174]
+k_beta = [8.95544201799072, 25.105444424220387, 17.982645794347498, 9.695802325558363, 5.097266130416366, 7.072766943625728, 28.42222963004282, 19.53560367390865, 26.785539971006223, 8.403630082046032]
+
+k_alpha_plot = np.sqrt(np.array(k_alpha)*1e3*1.6e-19 / (6.63e-34 * 3e8))
+k_beta_plot = np.sqrt(np.array(k_beta)*1e3*1.6e-19 / (6.63e-34 * 3e8))
+
+#atomic_numbers_k = ['Cu', 'Ag', 'Zr', 'Zn','Ti', 'Fe', 'Sn','Mo', 'In', 'Ni']
+atomic_numbers_k = [29, 47, 40, 30, 21, 26, 50, 42, 49, 28]
+
+l_alpha = [11.357273788806593, 14.856025841377411, 13.453359107827593]
+l_beta = [9.792511388784876, 12.588079827993488, 11.485332462914121]
+l_gamma = [8.579884420912107, 10.59672656896353, 9.77571560579842]
+
+l_alpha_plot = np.sqrt(np.array(l_alpha)*1e3*1.6e-19 / (6.63e-34 * 3e8))
+l_beta_plot = np.sqrt(np.array(l_beta)*1e3*1.6e-19 / (6.63e-34 * 3e8))
+l_gamma_plot = np.sqrt(np.array(l_gamma)*1e3*1.6e-19 / (6.63e-34 * 3e8))
+
+
+atomic_numbers_l = [74, 82, 79]
+l_elements_high_res = ['W', 'Pb', 'Au']
+
+atomic_numbers_k_sorted, k_alpha_plot_sorted, k_beta_plot_sorted = zip(*sorted(zip(atomic_numbers_k, k_alpha_plot,k_beta_plot)))
+atomic_numbers_l_sorted, l_alpha_plot_sorted, l_beta_plot_sorted,l_gamma_plot_sorted = zip(*sorted(zip(atomic_numbers_l, l_alpha_plot,l_beta_plot,l_gamma_plot)))
+
+atomic_numbers_k_sorted = np.array(atomic_numbers_k_sorted)
+atomic_numbers_l_sorted = np.array(atomic_numbers_l_sorted)
+k_alpha_plot_sorted = np.array(k_alpha_plot_sorted)
+k_beta_plot_sorted = np.array(k_beta_plot_sorted)
+l_alpha_plot_sorted = np.array(l_alpha_plot_sorted)
+l_beta_plot_sorted = np.array(l_beta_plot_sorted)
+l_gamma_plot_sorted = np.array(l_gamma_plot_sorted)
+
+k_alpha_plot_sorted_30 = k_alpha_plot_sorted[atomic_numbers_k_sorted >= 30]
+k_beta_plot_sorted_30 = k_beta_plot_sorted[atomic_numbers_k_sorted >= 30]
+atomic_numbers_k_sorted_30 = atomic_numbers_k_sorted[atomic_numbers_k_sorted >= 30]
+
+l_alpha_plot_sorted_30 = l_alpha_plot_sorted[atomic_numbers_l_sorted >= 30]
+l_beta_plot_sorted_30 = l_beta_plot_sorted[atomic_numbers_l_sorted >= 30]
+l_gamma_plot_sorted_30 = l_gamma_plot_sorted[atomic_numbers_l_sorted >= 30]
+atomic_numbers_l_sorted_30 = atomic_numbers_l_sorted[atomic_numbers_l_sorted >= 30]
+
+k_grad_alpha_30_guess = (k_alpha_plot_sorted_30[1]-k_alpha_plot_sorted_30[0]) / (atomic_numbers_k_sorted_30[1]-atomic_numbers_k_sorted_30[0])
+k_c_alpha_30_guess = k_alpha_plot_sorted_30[1] - k_grad_alpha_30_guess * atomic_numbers_k_sorted_30[1]
+k_alpha_30_guess = [k_grad_alpha_30_guess,k_c_alpha_30_guess]
+
+k_grad_beta_30_guess = (k_beta_plot_sorted_30[1]-k_beta_plot_sorted_30[0]) / (atomic_numbers_k_sorted_30[1]-atomic_numbers_k_sorted_30[0])
+k_c_beta_30_guess = k_beta_plot_sorted_30[1] - k_grad_beta_30_guess * atomic_numbers_k_sorted_30[1]
+k_beta_30_guess = [k_grad_beta_30_guess,k_c_beta_30_guess]
+
+# ---- L Lines ----
+
+l_grad_alpha_30_guess = (l_alpha_plot_sorted_30[1]-l_alpha_plot_sorted_30[0]) / (atomic_numbers_l_sorted_30[1]-atomic_numbers_l_sorted_30[0])
+l_c_alpha_30_guess = l_alpha_plot_sorted_30[1] - l_grad_alpha_30_guess * atomic_numbers_l_sorted_30[1]
+l_alpha_30_guess = [l_grad_alpha_30_guess,l_c_alpha_30_guess]
+
+l_grad_beta_30_guess = (l_beta_plot_sorted_30[1]-l_beta_plot_sorted_30[0]) / (atomic_numbers_l_sorted_30[1]-atomic_numbers_l_sorted_30[0])
+l_c_beta_30_guess = l_beta_plot_sorted_30[1] - l_grad_beta_30_guess * atomic_numbers_l_sorted_30[1]
+l_beta_30_guess = [l_grad_beta_30_guess,l_c_beta_30_guess]
+
+l_grad_gamma_30_guess = (l_gamma_plot_sorted_30[1]-l_gamma_plot_sorted_30[0]) / (atomic_numbers_l_sorted_30[1]-atomic_numbers_l_sorted_30[0])
+l_c_gamma_30_guess = l_gamma_plot_sorted_30[1] - l_grad_gamma_30_guess * atomic_numbers_l_sorted_30[1]
+l_gamma_30_guess = [l_grad_gamma_30_guess,l_c_gamma_30_guess]
+
+k_alpha_30_fit,k_alpha_30_cov = spo.curve_fit(straight_line,atomic_numbers_k_sorted_30,k_alpha_plot_sorted_30,k_alpha_30_guess)
+k_beta_30_fit,k_beta_30_cov = spo.curve_fit(straight_line,atomic_numbers_k_sorted_30,k_beta_plot_sorted_30,k_beta_30_guess)
+l_alpha_30_fit,l_alpha_30_cov = spo.curve_fit(straight_line,atomic_numbers_l_sorted_30,l_alpha_plot_sorted_30,l_alpha_30_guess)
+l_beta_30_fit,l_beta_30_cov = spo.curve_fit(straight_line,atomic_numbers_l_sorted_30,l_beta_plot_sorted_30,l_beta_30_guess)
+l_gamma_30_fit,l_gamma_30_cov = spo.curve_fit(straight_line,atomic_numbers_l_sorted_30,l_gamma_plot_sorted_30,l_gamma_30_guess)
+
+k_domain_invalid = np.arange(min(atomic_numbers_k_sorted),31,1)
+k_domain_valid = np.arange(30,max(atomic_numbers_k_sorted)+1,1)
+
+l_domain_invalid = np.arange(min(atomic_numbers_k_sorted),min(atomic_numbers_l_sorted)+1,1)
+l_domain_valid = np.arange(min(atomic_numbers_l_sorted),max(atomic_numbers_l_sorted)+1,1)
+
+plt.scatter(atomic_numbers_k_sorted,k_alpha_plot_sorted, label = r'$k_{\alpha} Data$', color = u'#1f77b4')
+plt.plot(k_domain_valid,straight_line(k_domain_valid,*k_alpha_30_fit), label = r'$k_{\alpha} fit$', color = u'#1f77b4')
+plt.plot(k_domain_invalid,straight_line(k_domain_invalid,*k_alpha_30_fit), color = u'#1f77b4', linestyle = '--')
+plt.scatter(atomic_numbers_k_sorted,k_beta_plot_sorted, label = r'$k_{\beta}$',color = u'#ff7f0e')
+plt.plot(k_domain_valid,straight_line(k_domain_valid,*k_beta_30_fit), label = r'$k_{\beta} fit$', color = u'#ff7f0e')
+plt.plot(k_domain_invalid,straight_line(k_domain_invalid,*k_beta_30_fit), color = u'#ff7f0e', linestyle = '--')
+plt.scatter(atomic_numbers_l_sorted,l_alpha_plot_sorted, label = r'$l_{\alpha}$', color = u'#2ca02c')
+plt.plot(l_domain_valid,straight_line(l_domain_valid,*l_alpha_30_fit), label = r'$l_{\alpha} fit$', color = u'#2ca02c')
+plt.plot(l_domain_invalid,straight_line(l_domain_invalid,*l_alpha_30_fit), color = u'#2ca02c', linestyle = '--')
+plt.scatter(atomic_numbers_l_sorted,l_beta_plot_sorted, label = r'$l_{\beta}$', color = u'#d62728')
+plt.plot(l_domain_valid,straight_line(l_domain_valid,*l_beta_30_fit), label = r'$l_{\beta} fit$', color = u'#d62728')
+plt.plot(l_domain_invalid,straight_line(l_domain_invalid,*l_beta_30_fit), color = u'#d62728', linestyle = '--')
+plt.scatter(atomic_numbers_l_sorted,l_gamma_plot_sorted, label = r'$l_{\gamma}$', color = u'#9467bd')
+plt.plot(l_domain_valid,straight_line(l_domain_valid,*l_gamma_30_fit), label = r'$l_{\gamma} fit$', color = u'#9467bd')
+plt.plot(l_domain_invalid,straight_line(l_domain_invalid,*l_gamma_30_fit), color = u'#9467bd', linestyle = '--')
+
+
+k_alpha_rydberg = cal_rydberg(k_alpha_30_fit[0],3/4)
+k_beta_rydberg = cal_rydberg(k_beta_30_fit[0],8/9)
+l_alpha_rydberg = cal_rydberg(l_alpha_30_fit[0],5/36)
+l_beta_rydberg = cal_rydberg(l_beta_30_fit[0],3/16)
+l_gamma_rydberg = cal_rydberg(l_gamma_30_fit[0],21/100)
+print(l_beta_30_fit[0])
+print(l_alpha_30_fit)
+print(f"Percentage Differnce k_alpha: {100*(R_0 - k_alpha_rydberg)/R_0} with value {k_alpha_rydberg}")
+print(f"Percentage Differnce k_beta: {100*(R_0 - k_beta_rydberg)/R_0 }with value {k_beta_rydberg}")
+print(f"Percentage Differnce l_alpha: {100*(R_0 - l_alpha_rydberg)/R_0} with value {l_alpha_rydberg}")
+print(f"Percentage Differnce l_beta: {100*(R_0 - l_beta_rydberg)/R_0} with value {l_beta_rydberg}")
+print(f"Percentage Differnce l_gamma: {100*(R_0 - l_gamma_rydberg)/R_0} with value {l_gamma_rydberg}")
+
+
+
+
+
+plt.legend()
+plt.show()
+
+
