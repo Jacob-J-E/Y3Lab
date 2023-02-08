533ec139bfac01f6d965c1bd9d51868751730827
Ganel <Ganelreddy1@gmail.com>
2023-02-08 15:01:26 +0000

    Initial Compton

--- Compton_Effect/Session_3_08_02_2023/background_removal_inital.py
+++ Compton_Effect/Session_3_08_02_2023/background_removal_inital.py
@@ -4,9 +4,146 @@ import scipy.optimize as spo
 import pandas as pd
 import math
 from scipy.signal import argrelextrema
+from scipy.signal import savgol_filter
 import xraydb
 import mplhep as hep
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.mixture import GaussianMixture
+
 hep.style.use("ATLAS")
 
+#Gain used:- 3 
 
-data_low_res = pd.read_csv(r"X-Ray\Data\24-01-2023\Plates Mo Source.csv",skiprows=0)
+data = pd.read_csv(r"Compton_Effect\Session_3_08_02_2023\Background and Cs137.csv",skiprows=0)
+
+print(data.columns)
+
+
+def gaussian(x, a, b, c):
+    return (a * np.exp(-((x - b) ** 2) / (2 * c ** 2)))
+
+energy = data['E_1 / keV']
+bin_num = [i for i in range(0,len(energy))]
+cs = data['Cs137']
+background = data['Background']
+
+background_removed_cs = cs - background
+savgol_filter = savgol_filter(background_removed_cs,101,3)
+X = savgol_filter.reshape(-1, 1)
+# plt.figure(1)
+# plt.plot(bin_num,cs, label = 'Cs Source')
+# plt.plot(bin_num,background, label = 'Background')
+# plt.plot(bin_num,background_removed_cs, label = 'Background Removed Cs')
+# plt.plot(bin_num,savgol_filter, label = 'Background Removed Cs')
+
+# guess_e1 = [1230,700,0.01]
+# params_e1, cov_e1 = spo.curve_fit(gaussian,bin_num,savgol_filter,guess_e1)
+# print(params_e1)
+
+# plt.plot(bin_num,gaussian(bin_num,*params_e1))
+# plt.legend()
+# plt.show()
+
+#----------------------------------------------------------------------
+# This function adjusts matplotlib settings for a uniform feel in the textbook.
+# Note that with usetex=True, fonts are rendered with LaTeX.  This may
+# result in an error if LaTeX is not installed on your system.  In that case,
+# you can set usetex to False.
+
+
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn.mixture import GaussianMixture
+
+#----------------------------------------------------------------------
+# This function adjusts matplotlib settings for a uniform feel in the textbook.
+# Note that with usetex=True, fonts are rendered with LaTeX.  This may
+# result in an error if LaTeX is not installed on your system.  In that case,
+# you can set usetex to False.
+if "setup_text_plots" not in globals():
+    from astroML.plotting import setup_text_plots
+setup_text_plots(fontsize=8, usetex=True)
+
+#------------------------------------------------------------
+# Set up the dataset.
+#  We'll create our dataset by drawing samples from Gaussians.
+
+
+#------------------------------------------------------------
+# Learn the best-fit GaussianMixture models
+#  Here we'll use scikit-learn's GaussianMixture model. The fit() method
+#  uses an Expectation-Maximization approach to find the best
+#  mixture of Gaussians for the data
+
+# fit models with 1-10 components
+N = np.arange(1, 11)
+models = [None for i in range(len(N))]
+
+for i in range(len(N)):
+    models[i] = GaussianMixture(N[i]).fit(X)
+
+# compute the AIC and the BIC
+AIC = [m.aic(X) for m in models]
+BIC = [m.bic(X) for m in models]
+
+#------------------------------------------------------------
+# Plot the results
+#  We'll use three panels:
+#   1) data + best-fit mixture
+#   2) AIC and BIC vs number of components
+#   3) probability that a point came from each component
+
+fig = plt.figure(figsize=(5, 1.7))
+fig.subplots_adjust(left=0.12, right=0.97,
+                    bottom=0.21, top=0.9, wspace=0.5)
+
+
+# plot 1: data + best-fit mixture
+ax = fig.add_subplot(131)
+M_best = models[np.argmin(AIC)]
+
+x = np.linspace(-6, 6, 1000)
+logprob = M_best.score_samples(x.reshape(-1, 1))
+responsibilities = M_best.predict_proba(x.reshape(-1, 1))
+pdf = np.exp(logprob)
+pdf_individual = responsibilities * pdf[:, np.newaxis]
+
+ax.hist(X, 30, density=True, histtype='stepfilled', alpha=0.4)
+ax.plot(x, pdf, '-k')
+ax.plot(x, pdf_individual, '--k')
+ax.text(0.04, 0.96, "Best-fit Mixture",
+        ha='left', va='top', transform=ax.transAxes)
+ax.set_xlabel('$x$')
+ax.set_ylabel('$p(x)$')
+
+
+# plot 2: AIC and BIC
+ax = fig.add_subplot(132)
+ax.plot(N, AIC, '-k', label='AIC')
+ax.plot(N, BIC, '--k', label='BIC')
+ax.set_xlabel('n. components')
+ax.set_ylabel('information criterion')
+ax.legend(loc=2)
+
+
+# plot 3: posterior probabilities for each component
+ax = fig.add_subplot(133)
+
+p = responsibilities
+p = p[:, (1, 0, 2)]  # rearrange order so the plot looks better
+p = p.cumsum(1).T
+
+ax.fill_between(x, 0, p[0], color='gray', alpha=0.3)
+ax.fill_between(x, p[0], p[1], color='gray', alpha=0.5)
+ax.fill_between(x, p[1], 1, color='gray', alpha=0.7)
+ax.set_xlim(-6, 6)
+ax.set_ylim(0, 1)
+ax.set_xlabel('$x$')
+ax.set_ylabel(r'$p({\rm class}|x)$')
+
+ax.text(-5, 0.3, 'class 1', rotation='vertical')
+ax.text(0, 0.5, 'class 2', rotation='vertical')
+ax.text(3, 0.3, 'class 3', rotation='vertical')
+
+plt.show()
