d8d9df5ef83a115b5c72c3c1dedd738d4b44f950
Jacob-J-E <jacobedginton@gmail.com>
2023-02-18 14:02:46 +0000

    Fixed all geometry looping

--- Compton_Effect/Imaging/all_geometry.py
+++ Compton_Effect/Imaging/all_geometry.py
@@ -21,33 +21,29 @@ def geo_difference(theory,exp):
 
  
 
-X_bounds = [1,20]
-Y_bounds = [1,11]
+X_bounds = [5,30]
+Y_bounds = [1,20]
 geometries = []
 
 alpha_temp = []
 s_temp = []
 d_temp = []
 
+valid_geometry = []
 for x in range(X_bounds[0],X_bounds[1]+1):
     for y in range(Y_bounds[0],Y_bounds[1]+1):
-        for s in range(1,X_bounds[0]):
-            for d in range(X_bounds[1]+1, 30):
+        for s in range(1,X_bounds[0]+10):
+            for d in range(X_bounds[1]+1, 30+10):
                 # alpha = alpha_calc(x,y,d,s)
                 # geometries.append([s,d,alpha,x,y])
-
-                if x == 6 and y == 4:
-                    alpha_temp.append(alpha_calc(x,y,d,s))
-                    s_temp.append(s)
-                    d_temp.append(d)
-
-#print(geometries)
-print(alpha_temp)
-print(s_temp)
-print(d_temp)
+                # print(x)
+                if (x == 6) and (y==4):
+                    print("AHh")
+                    valid_alpha = alpha_calc(x,y,d,s)
+                    valid_geometry.append([x,y,d,s,valid_alpha])
 
 
+print(valid_geometry)
 
-# print(alpha_calc(6,4,10,2))
 
-exp_geo = []
+# exp_geo = []
