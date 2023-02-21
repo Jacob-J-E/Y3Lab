9080ab1384862ac8e56f5739e39a53ed2c4051f2
Jacob-J-E <jacobedginton@gmail.com>
2023-02-21 11:36:07 +0000

    UMAP with HDBScan

--- Compton_Effect/Imaging/ALT_all_geometry_umap.py
+++ Compton_Effect/Imaging/ALT_all_geometry_umap.py
@@ -6,7 +6,7 @@ import scipy.optimize as spo
 import scipy.signal as ssp
 import umap
 import umap.plot
-
+import hdbscan
 hep.style.use("CMS")
 
 def scatter_difference(coordinates: list, alpha: np.array, d:np.array, s:np.array):
@@ -49,11 +49,15 @@ six_alpha_temp = []
 six_s_temp = []
 six_d_temp = []
 six_label = []
+six_x = []
+six_y = []
 
 two_alpha_temp = []
 two_s_temp = []
 two_d_temp = []
 two_label = []
+two_x = []
+two_y = []
 
 valid_geometry = []
 for x in range(X_bounds[0],X_bounds[1]+1):
@@ -70,6 +74,8 @@ for x in range(X_bounds[0],X_bounds[1]+1):
                     six_s_temp.append(s)
                     six_alpha_temp.append(valid_alpha)
                     six_label.append(6)
+                    six_x.append(x)
+                    six_y.append(y)
 
                 if (x == 2) and (y==7):
                     valid_alpha = alpha_calc(x,y,d,s)
@@ -78,6 +84,18 @@ for x in range(X_bounds[0],X_bounds[1]+1):
                     two_s_temp.append(s)
                     two_alpha_temp.append(valid_alpha)
                     two_label.append(2)
+                    two_x.append(x)
+                    two_y.append(y)
+
+
+
+two_x =  (two_d_temp[0]-two_s_temp[0])/2
+six_x =  (six_d_temp[0]-six_s_temp[0])/2
+
+two_y = ((two_d_temp[0]-two_s_temp[0]))/(np.tan(two_alpha_temp[0]))
+six_y = ((six_d_temp[0]-six_s_temp[0]))/(np.tan(six_alpha_temp[0]))
+
+
 
 print(f'length of two alpha {len(two_alpha_temp)}')
 print(f'length of six alpha {len(six_alpha_temp)}')
@@ -88,20 +106,101 @@ combined_s = np.array(two_s_temp + six_s_temp)
 combined_d = np.array(two_d_temp + six_d_temp)
 combined_labels = six_label + two_label
 
+combined_x = []
+combined_y = []
+for i in range(0,len(combined_s)):
+    x_guess = (combined_d[i]-combined_s[i])/2
+    y_guess = ((combined_d[i]-combined_s[i]))/(np.tan(combined_alpha[0]))
+    result = spo.basinhopping(func=scatter_difference, niter=100, x0=[x_guess,y_guess], T=0, minimizer_kwargs = {"args":(combined_alpha[i],combined_d[i],combined_s[i]),"method":'BFGS',"bounds":([0,20],[0, 20])})
+    
+    if result.x[0] < 0:
+        combined_x.append(0)
+    else:
+        combined_x.append(result.x[0])
+    if result.x[1] < 0:
+        combined_y.append(0)
+    else:
+        combined_y.append(result.x[1])
+
+    print("Iteration ",i," of ", len(combined_s))
 
 
-print(combined_alpha)
-
-d = {'combined_s':combined_s,'combined_d':combined_d,'combined_alpha':combined_alpha,'labels':combined_labels}
+d = {'combined_s':combined_s,'combined_d':combined_d,'combined_alpha':combined_alpha,'x':combined_x,'y':combined_y,'labels':combined_labels}
 dataframe = pd.DataFrame(data = d)
 dataframe = dataframe.sample(frac=1).reset_index(drop=True)
 y_train = dataframe.labels
 X_train = dataframe.drop(columns='labels')
 
+plt.scatter(combined_x,combined_y)
+plt.show()
 
-trans = umap.UMAP(n_neighbors=150, random_state=50, n_components=2, min_dist=1).fit(X_train)
-umap.plot.points(trans,labels=y_train)
-umap.plot.plt.show()
+"""
+Old UMAP
+"""
+# trans = umap.UMAP(n_neighbors=100, random_state=30, n_components=2, min_dist=1).fit(X_train)
+# # umap.plot.points(trans,labels=y_train)
+# # umap.plot.plt.grid()
+# # umap.plot.plt.show()
+# fig,ax = plt.subplots(1,2)
+# ax[0].scatter(trans.embedding_[:, 0], trans.embedding_[:, 1], s= 20, c=y_train, cmap='Spectral')
+# x_ = trans.embedding_[:, 0]
+# y_ = trans.embedding_[:, 1]
+# # plt.title('Embedding of the training set by UMAP', fontsize=24)
+# # plt.grid()
+# ax[1].scatter(x_[x_<5],y_[x_<5],color='black',marker='x',s=10)
+# ax[1].scatter(x_[x_>5],y_[x_>5],color='red',marker='o',s=10)
+
+# plt.show()
+
+
+
+"""
+Clusters
+"""
+standard_embedding = umap.UMAP(
+    n_neighbors=50,
+    min_dist=1,
+    n_components=2,
+    random_state=42,
+).fit_transform(X_train)
+
+clusterable_embedding = umap.UMAP(
+    n_neighbors=50,
+    min_dist=1,
+    n_components=2,
+    random_state=42,
+).fit_transform(X_train)
+
+plt.scatter(clusterable_embedding[:, 0], clusterable_embedding[:, 1],
+            c=y_train, s=20, cmap='Spectral')
+plt.show()
+
+labels = hdbscan.HDBSCAN(
+    min_samples=10,
+    min_cluster_size=5,
+).fit_predict(clusterable_embedding)
+
+print("AHH",labels)
+clustered = (labels >= 0)
+
+plt.scatter(standard_embedding[~clustered, 0],
+            standard_embedding[~clustered, 1],
+            color=(0.5, 0.5, 0.5),
+            s=20,
+            alpha=0.5)
+
+plt.scatter(standard_embedding[clustered, 0],
+            standard_embedding[clustered, 1],
+            c=labels[clustered],
+            s=20,
+            cmap='Spectral')
+
+plt.show()
+
+
+
+
+
 # fig = plt.figure()
 # ax = plt.axes(projection='3d')
 # ax.scatter3D(trans.embedding_[:, 0], trans.embedding_[:, 1], trans.embedding_[:, 2],s= 10, c=y_train)
@@ -110,10 +209,6 @@ umap.plot.plt.show()
 # plt.title('Embedding of the training set by UMAP', fontsize=24)
 # plt.grid()
 # plt.show()
-
-"""
-# DB Scan
-"""
 
 
 
--- Compton_Effect/Imaging/UMAP_test.py
+++ Compton_Effect/Imaging/UMAP_test.py
@@ -1,17 +1,50 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import seaborn as sns
-import umap
-sns.set(style='white', context='poster', rc={'figure.figsize':(14,10)})
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
 
-np.random.seed(42)
+from umap import UMAP
 
-data = np.random.rand(800, 4)
+# Make a toy dataset
+X, y = make_classification(
+    n_samples=1000,
+    n_features=300,
+    n_informative=250,
+    n_redundant=0,
+    n_repeated=0,
+    n_classes=2,
+    random_state=1212,
+)
 
-fit = umap.UMAP()
-u = fit.fit_transform(data)
+# Split the dataset into a training set and a test set
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-plt.scatter(u[:,0], u[:,1], c=data)
-plt.title('UMAP embedding of random colours')
-plt.show()
+# Classification with a linear SVM
+svc = LinearSVC(dual=False, random_state=123)
+params_grid = {"C": [10 ** k for k in range(-3, 4)]}
+clf = GridSearchCV(svc, params_grid)
+clf.fit(X_train, y_train)
+print(
+    "Accuracy on the test set with raw data: {:.3f}".format(clf.score(X_test, y_test))
+)
+
+# Transformation with UMAP followed by classification with a linear SVM
+umap = UMAP(random_state=456)
+pipeline = Pipeline([("umap", umap), ("svc", svc)])
+params_grid_pipeline = {
+    "umap__n_neighbors": [5, 20],
+    "umap__n_components": [15, 25, 50],
+    "svc__C": [10 ** k for k in range(-3, 4)],
+}
+
+
+clf_pipeline = GridSearchCV(pipeline, params_grid_pipeline)
+clf_pipeline.fit(X_train, y_train)
+print(
+    "Accuracy on the test set with UMAP transformation: {:.3f}".format(
+        clf_pipeline.score(X_test, y_test)
+    )
+)
+
