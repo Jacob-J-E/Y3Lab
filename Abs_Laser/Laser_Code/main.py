f213e134984b5811c47715d42e1a15c476f87ff2
Ganel <Ganelreddy1@gmail.com>
2023-04-08 07:47:36 +0100

    Hyperfine structure graphs

--- Abs_Laser/Laser_Code/main.py
+++ Abs_Laser/Laser_Code/main.py
@@ -21,19 +21,20 @@ from scipy.ndimage import convolve1d
 from alive_progress import alive_bar
 
 
-max_poly_order = 5
-min_poly_order = 2
+# Min Max Values are inclusive
+max_poly_order = 4
+min_poly_order = 4
 if min_poly_order > max_poly_order:
     print('Make sure max poly order is higher or equal to min poly order')
     exit()
-elif min_poly_order < 1:
-    print('Please put a coeff greater than or equal to 1')
+elif min_poly_order < 2:
+    print('Please put a coeff greater than or equal to 2')
     exit()
 
 
 R_1_R_2 = 0.995*0.995
 F = 0.5 * (np.pi * (R_1_R_2)**(1/4))/(1-(R_1_R_2)**(1/2))
-fsr_theoretical = 3e8/(2*20e-2)
+fsr_theoretical = 3e8/(4*19e-2)
 
 
 print('********CONSTANTS***********')
@@ -114,6 +115,12 @@ x_axis = x_axis[(x_axis > -0.01749) & (x_axis < 0.01573)]
 x_axis = x_axis[::-1]
 x_axis, c3, c4,c1,c1_B = zip(*sorted(zip(x_axis, c3, c4,c1,c1_B )))
 
+c1_B = np.array(c1_B)
+c3 = np.array(c3)
+c4 = np.array(c4)
+c1 = np.array(c1)
+x_axis = np.array(x_axis)
+
 #NORMALIZING X AXIS -------------------------------------------------------------------------------
 length_of_xaxis = len(x_axis)
 normalized_x_axis = []
@@ -173,9 +180,9 @@ a_1_guess = para_straight[0]
 
 #Plotting modified airy function ----------------------------------------------------------------
 
-domain_airy_modified = np.linspace(min(normalized_x_axis),max(normalized_x_axis), 100000)
 
 array_of_coeffients = []
+b_values = []
 for i in range(min_poly_order,max_poly_order+1):
     poly_order = i 
     # ordering -> a_0,a_1, a_2, a_3, a_4, b_0, b_1)
@@ -187,40 +194,101 @@ for i in range(min_poly_order,max_poly_order+1):
     iterations = 100
     with alive_bar(iterations) as bar:
         for i in range(iterations):
+            bar()
             #bounds= ((0,0,0,0,0,0,0),(np.inf,np.inf,np.inf,np.inf,np.inf,np.inf,np.inf))
             para_airy_modified, cov_airy_modified = curve_fit(airy_modified_function,normalized_x_axis,lorentzian_array,inital_guess_airy_modified)
             inital_guess_airy_modified = para_airy_modified
-            bar()
 
     a_coeffients = para_airy_modified[:poly_order+1]
+    b_coeff = [para_airy_modified[-2],para_airy_modified[-1]]
     array_of_coeffients.append(a_coeffients)
-    print(f'a coeffients {a_coeffients}')
+    b_values.append(b_coeff)
+    print(f'COMPLETED BOOTSTRAPING with Poly Order {poly_order}')
+    print(f'Poly Order {poly_order} - a coeffients {a_coeffients}')
+    print(f'Poly Order {poly_order} - b coeffients {b_coeff}')
+
 
 plt.figure()
-plt.plot(domain_airy_modified,airy_modified_function(domain_airy_modified,*para_airy_modified), label =  "Fitted" )
+domain_airy_modified = np.linspace(min(normalized_x_axis),max(normalized_x_axis), 100000)
+
+plt.title('Fitted FP using Modified Airy Function')
+for i in range(len(array_of_coeffients)):
+    para = list(array_of_coeffients[i]) + list(b_values[i])
+    plt.plot(domain_airy_modified,airy_modified_function(domain_airy_modified,*para), label =  f"Fitted Airy (f(x) poly order {min_poly_order + i})" )
 plt.plot(normalized_x_axis,lorentzian_array, label =  "Data" )
+plt.legend()
 
 plt.figure()
+plt.title('Frequency Scaled FP')
 for i in range(len(array_of_coeffients)):
     freqency_array = f(normalized_x_axis,*array_of_coeffients[i])
     plt.plot(freqency_array,lorentzian_array, label =  f"Frequency Scaling (poly order {min_poly_order + i})" )
-
+plt.xticks(np.arange(0, max(freqency_array)+fsr_theoretical, fsr_theoretical))
+plt.legend()
 
 
 
 plt.figure()
+plt.title('Frequency Scaled FP Peak Spacings')
+colors = plt.cm.rainbow(np.linspace(0, 1, len(array_of_coeffients)))
 for i in range(len(array_of_coeffients)):
     freqency_array = f(normalized_x_axis,*array_of_coeffients[i])
     freq_peaks = freqency_array[peaks]
     freq_peaks = freq_peaks[1:]
+    mid_val = (freq_peaks[1:] + freq_peaks[:-1])/2
     spacing_freq = np.diff(freq_peaks)
-    plt.scatter(freq_peaks[1:],spacing_freq, label = f'Frequency FP spacing difference (poly order {min_poly_order + i})')
+    plt.scatter(mid_val,spacing_freq, label = f'Frequency FP spacing difference (poly order {min_poly_order + i})', color = colors[i])
+    plt.axhline(max(spacing_freq), color = colors[i])
+    plt.axhline(min(spacing_freq), color = colors[i])
+plt.legend()
 
 plt.figure()
-plt.scatter(x_axis_peaks[1:],spacing, label = 'time scale')
-
-
-
+plt.title('Fitted FP Lorentzian (Original Dataset) Peak Spacings')
+mid_point = (x_axis_peaks[1:] + x_axis_peaks[:-1]) / 2
+plt.scatter(mid_point,spacing, label = 'Time scale')
 plt.legend()
-plt.show()
 
+
+
+#Plotting the Broadened Absorption Spectra----------------------------------------------------------------
+plt.figure()
+plt.title('Frequency Scaled Broadened Absorption Spectra')
+for i in range(len(array_of_coeffients)):
+    peaks_fine, _= find_peaks(-1*c1_B, distance=8000)
+    c1_b_grouped_peaks =c1_B[peaks_fine]
+    freqency_array = f(normalized_x_axis,*array_of_coeffients[i])
+    freq_peaks = freqency_array[peaks_fine]
+
+    centers_fine = [freq_peaks[1],freq_peaks[2],freq_peaks[4],freq_peaks[6]]
+    amplitude_fine = [c1_b_grouped_peaks[1],c1_b_grouped_peaks[2],c1_b_grouped_peaks[4],c1_b_grouped_peaks[6]]
+
+    Rb_85_Ground_State_Difference = (freq_peaks[4]-freq_peaks[2])/1e9
+    Rb_87_Ground_State_Difference = (freq_peaks[6]-freq_peaks[1])/1e9
+
+    Rb_85_Ground_State_Difference_THEORY = 3.0357324390
+    Rb_87_Ground_State_Difference_THEORY = 6.834682610904290
+
+    Rb_85_Ground_State_Difference_THEORY_PD = ((Rb_85_Ground_State_Difference_THEORY - Rb_85_Ground_State_Difference) / Rb_85_Ground_State_Difference_THEORY) * 100
+    Rb_87_Ground_State_Difference_THEORY_PD = ((Rb_87_Ground_State_Difference_THEORY - Rb_87_Ground_State_Difference) / Rb_87_Ground_State_Difference_THEORY) * 100
+  
+    print('************************************')
+    print(f'Poly Order: {min_poly_order + i}')
+    print(f'Hyperfine Structure Rb 85 Ground State Difference: {Rb_85_Ground_State_Difference:.4g}, Theory P.D: {Rb_85_Ground_State_Difference_THEORY_PD:.4g}%')
+    print(f'Hyperfine Structure Rb 87 Ground State Difference: {Rb_87_Ground_State_Difference:.4g}, Theory P.D: {Rb_87_Ground_State_Difference_THEORY_PD:.4g}%')
+
+    plt.plot(freqency_array,c1_B, label = f'Data (poly order {min_poly_order + i})')
+    plt.scatter(centers_fine,amplitude_fine, marker= 'x', label = f'Scatter (poly order {min_poly_order + i})')
+plt.legend()
+
+
+plt.figure()
+plt.title('Frequency Scaled Hyperfine Structure')
+for i in range(len(array_of_coeffients)):
+    freqency_array = f(normalized_x_axis,*array_of_coeffients[i])
+    hyper_fine_structure = c1-c1_B
+    hyper_fine_structure = hyper_fine_structure - min(hyper_fine_structure)
+    hyper_fine_structure = hyper_fine_structure/max(hyper_fine_structure)
+    plt.plot(freqency_array,hyper_fine_structure,label = f'Data Hyperfine Structure (poly order {min_poly_order + i})')
+plt.legend()
+
+plt.show()
